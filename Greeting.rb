e3ec1c301a21c7823ba958ef261c167f7a23c081
marsh-sb <enotaku5526@gmail.com>
2021-03-31 20:33:42 +0900

    add Lesson06

---
+++ Greeting.rb
@@ -0,0 +1,9 @@
+print "good morning"
+
+print "good afternoon"
+
+print "good evening"
+
+puts "Good morning".index("d")
+puts "Good Morning".length
+puts "Good Morning".class
---
+++ Lesson06_01.rb
@@ -0,0 +1,27 @@
+num = 1
+num01 = 2
+num_01 = 3
+# num-01 = 4
+
+p num
+p num01
+p num_01
+# p num-01
+
+NUM = 1
+Num = 2
+
+p NUM
+p Num
+
+p 10.div(5)
+p 10.remainder(3)
+
+p 10.class
+p 10.0.class
+
+p (10>1)
+p (10>1).class
+
+p (10<1)
+p (10<1).class
