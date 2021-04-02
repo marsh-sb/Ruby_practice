1027515944c988432001df7e43b18b7c1d0fd2c1
marsh-sb <enotaku5526@gmail.com>
2021-04-02 20:49:26 +0900

    add Lesson8

---
+++ Lesson08_02.rb
@@ -0,0 +1,9 @@
+a = Array.new(3)
+
+a[0] = "Sato"
+a[1] = "Suzuki"
+a[2] = "Takahashi"
+
+p a[0]
+p a[1]
+p a[2]
---
+++ Lesson08_03.rb
@@ -0,0 +1,14 @@
+a = ["Sato", "Suzuki", "Takahashi"]
+
+a[1] = "Tanaka"
+
+p a[0]
+p a[1]
+p a[2]
+
+arr = [ ["Sato", "Suzuki"], ["Takahashi", "Tanaka"] ]
+
+p arr[0][0]
+p arr[0][1]
+p arr[1][0]
+p arr[1][1]
