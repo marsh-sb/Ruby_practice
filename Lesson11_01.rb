0cd7e144c114bcbb6f7fb6f61ae08da70a710e0a
marsh-sb <enotaku5526@gmail.com>
2021-04-06 14:55:50 +0900

    add Lesson11

---
+++ Lesson11_01.rb
@@ -0,0 +1,17 @@
+for i in 0 .. 4
+  p i
+end
+
+for i in 0 .. 4
+  if i == 3
+    break
+  end
+  p i
+end
+
+for i in 0 .. 4
+  if i == 3
+    next
+  end
+  p i
+end
---
+++ Lesson11_02.rb
@@ -0,0 +1,5 @@
+for i in 0 .. 2
+  for j in 0 .. 2
+    p i.to_s + "-" + j.to_s
+  end
+end
